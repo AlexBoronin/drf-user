20d9def45d0a1fcf25ab531a07716ee8b0f27c0c
Aliaksei Baronin <zenia2285@mail.ru>
2023-01-16 14:40:14 +0300

    Adde 2 model (Project+ToDo) (#3)
    * pullrequest
    
    * adde 2 model
    
    Co-authored-by: AlexBoronin <acivinel@mail.ru>

--- webtodoservice/authors/models.py
+++ webtodoservice/authors/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from uuid import uuid4
 
+
 class Author(models.Model):
     id = models.UUIDField(default=uuid4, primary_key=True)
     username = models.CharField(max_length=64)
     firstname = models.CharField(max_length=64)
     lastname = models.CharField(max_length=64)
     email = models.EmailField()
+
+class ToDo(models.Model):
+    descript = models.TextField()
+    dedline = models.DateField()
+
+class Project(models.Model):
+    title = models.CharField(max_length=32)
+    executor = models.CharField(max_length=64)
+
+
