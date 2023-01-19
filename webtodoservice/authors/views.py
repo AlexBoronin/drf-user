92b45bd92dd911b82640ad0fb19a67d047d0fa39
AlexBoronin <acivinel@mail.ru>
2023-01-19 19:38:53 +0300

    added MyAPIView

--- webtodoservice/authors/views.py
+++ webtodoservice/authors/views.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Author, Project, ToDo
 from .serializers import AuthorModelSerializer, ProjectModelSerializer, ToDoModelSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
@@ -16,3 +17,11 @@ class ProjectModelViewSet(ModelViewSet):
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+
+class MyAPIView(APIView):
+
+    def get(self, request):
+        return Response({'data': 'GET SUCCESS'})
+
+    def get(self, request):
+        return Response({'data': 'POST SUCCESS'})
