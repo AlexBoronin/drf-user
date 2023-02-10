92d8d4715c53ef0514570bbf82b8ce20f892b7ac
Aliaksei Baronin <zenia2285@mail.ru>
2023-02-11 01:57:31 +0300

    common (#13)
    Co-authored-by: AlexBoronin <acivinel@mail.ru>

--- webtodoservice/authors/tests.py
+++ webtodoservice/authors/tests.py
@@ -1,3 +1,71 @@
 from django.test import TestCase
+import json
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+# from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import AuthorViewSet
+from .models import Author, Book
 
-# Create your tests here.
+
+class TestAuthorViewSet(TestCase):
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/authors/')
+        view = AuthorViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/', {'username': 'Александр', 'lastname': 'Пушкин'}, format='json')
+        view = AuthorViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/', {'username': 'Пушкин', 'birthday_year': 1799}, format='json')
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        force_authenticate(request, admin)
+        view = AuthorViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_detail(self):
+        author = Author.objects.create(username='Пушкин', birthday_year=1799)
+        print(author.username)
+        client = APIClient()
+        response = client.get(f'/api/authors/{author.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_edit_guest(self):
+        author = Author.objects.create(name='Пушкин', birthday_year=1799)
+        client = APIClient()
+        response = client.put(f'/api/authors/{author.id}/', {'name': 'Грин', 'birthday_year': 1880})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_edit_admin(self):
+        author = Author.objects.create(name='Пушкин', birthday_year=1799)
+        client = APIClient()
+        admin = User.objects.create_superuser('admin', 'admin')
+        client.login(username='admin', password='admin')
+        response = client.put(f'/api/authors/{author.id}/', {'name':'Грин','birthday_year': 1880})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        author = Author.objects.get(id=author.id)
+        self.assertEqual(author.name, 'Грин')
+        self.assertEqual(author.birthday_year, 1880)
+        client.logout()
+
+class TestMath(APISimpleTestCase):
+    def test_sqrt(self):
+        import math
+        self.assertEqual(math.sqrt(4), 2)
+        def test_edit_mixer(self):
+            book = mixer.blend(Books)
+            admin = User.objects.create_superuser('admin', 'admin')
+            self.client.login(username='admin', password='admin')
+            response = self.client.put(f'/api/books/{book.id}/', {'name': 'Руслан и Людмила', 'author': book.author.id})
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            book = Book.objects.get(id=book.id)
+            self.assertEqual(book.name, 'Руслан и Людмила')
